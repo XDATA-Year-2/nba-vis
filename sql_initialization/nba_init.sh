2ef18f412848674f669d9abfea4885eaa8d1fdea
Roni Choudhury <roni.choudhury@kitware.com>
2014-07-16 12:10:39 -0400

    Abstracting sqlite and python executables into env vars; defaults remain as they were

--- sql_initialization/nba_init.sh
+++ sql_initialization/nba_init.sh
@@ -1,19 +1,25 @@
-rm ../nba.sqlite;
+#!/bin/sh
 
-sqlite3 ../nba.sqlite <schema.sql;
+if [ -z "$PYTHON" ]; then
+    PYTHON=python
+fi
 
-python nba_db_populate.py;
+if [ -z "$SQLITE" ]; then
+    SQLITE=sqlite3
+fi
 
+rm ../nba.sqlite
 
-sqlite3 ../nba.sqlite <scores_view.sql;
-sqlite3 ../nba.sqlite <team_outcomes_view.sql;
+$SQLITE ../nba.sqlite <schema.sql
 
+$PYTHON nba_db_populate.py
 
-sqlite3 ../nba.sqlite <nbavis_player_stats_view.sql;
-sqlite3 ../nba.sqlite <nbavis_team_stats_view.sql;
-sqlite3 ../nba.sqlite <nbavis_team_players_view.sql;
-sqlite3 ../nba.sqlite <nbavis_teamlines_view.sql;
+$SQLITE ../nba.sqlite <scores_view.sql
+$SQLITE ../nba.sqlite <team_outcomes_view.sql
 
+$SQLITE ../nba.sqlite <nbavis_player_stats_view.sql
+$SQLITE ../nba.sqlite <nbavis_team_stats_view.sql
+$SQLITE ../nba.sqlite <nbavis_team_players_view.sql
+$SQLITE ../nba.sqlite <nbavis_teamlines_view.sql
 
-
-python nba_db_comments_analysis.py;
+$PYTHON nba_db_comments_analysis.py
