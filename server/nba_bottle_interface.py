733a74f14bbabb9014c71501cfb78b728fa214fe
Roni Choudhury <roni.choudhury@kitware.com>
2014-07-17 11:08:15 -0400

    Abstracting server side services from bottle script

--- server/nba_bottle_interface.py
+++ server/nba_bottle_interface.py
@@ -1,4 +1,5 @@
 from bottle import hook, response, route, run, static_file
+import services
 import sqlite3
 import json
 import time
@@ -19,56 +20,23 @@ c = conn.cursor()
 
 @route('/www/hello')
 def hello():
-  return "Hello World!"
+    return services.hello()
 
 @route('/www/seasons')
 def seasons():
-  seasons = c.execute('select * from SEASONS order by SEASON')
-  ret = []
-  for s in seasons:
-    season = {}
-    season['id'] = s['season_id']
-    season['season'] = s['season']
-    ret.append(season)
-  return json.dumps(ret)
+    return services.seasons(cursor=c)
 
 @route('/www/teams')
 def teams():
-  teams = c.execute("select * from TEAMS order by TEAM_ABBREVIATION")
-  ret = []
-  for t in teams:
-    team = {}
-    team['id'] = t['team_id']
-    team['abbr'] = t['team_abbreviation']
-    team['city'] = t['team_city']
-    team['mascot'] = t['team_mascot']
-    ret.append(team)
-  return json.dumps(ret)
+    return services.teams(cursor=c)
 
 @route('/www/team_scores/<teamname>/<season>')
 def team_scores(teamname,season):
-  scores = c.execute('select * from scores where season='+season+' and (habbr="'+teamname+'" or vabbr="'+teamname+'") order by date;')
-  ret = []
-  for s in scores:
-    score = {}
-    home = score['home'] = (s['vabbr']!=teamname)
-    if home==True:
-      score['score'] = s['hpts']
-      score['opp_score'] = s['vpts']
-      score['opp'] = s['vabbr']
-    else:
-      score['score'] = s['vpts']
-      score['opp_score'] = s['hpts']
-      score['opp'] = s['habbr']
-    score['diff'] = score['score'] - score['opp_score']
-    score['gameid'] = s['game_id']
-    ret.append(score)
-  return json.dumps(ret)
+   return services.team_scores(teamname,season,cursor=c)
 
 @route('/www/notebook/<gameid>')
 def notebook(gameid):
-  notebook = c.execute('select notebook from games where game_id='+gameid).fetchone()
-  return notebook[0].replace('\n','\n<br/>')
+    return services.notebook(gameid,cursor=c)
 
 
 @route('/www/')
---
+++ server/services.py
@@ -0,0 +1,77 @@
+import cherrypy
+import json
+import sqlite3
+
+class sqlite_cursor:
+    def __init__(self, cursor):
+        if not hasattr(cherrypy.thread_data, "conn"):
+            cherrypy.thread_data.conn = sqlite3.connect(cherrypy.thread_data.modulepath + "/../nba.sqlite")
+            cherrypy.thread_data.conn.row_factory = sqlite3.Row
+
+        self.cursor = cursor
+        self.private_cursor = None
+
+    def __enter__(self):
+        if self.cursor is not None:
+            return self.cursor
+        else:
+            self.private_cursor = cherrypy.thread_data.conn.cursor()
+            return self.private_cursor
+
+    def __exit__(self, type, value, traceback):
+        if self.private_cursor:
+            self.private_cursor.close()
+            self.private_cursor = None
+
+
+def hello():
+  return "Hello World!"
+
+def seasons(cursor=None):
+    with sqlite_cursor(cursor) as c:
+        seasons = c.execute('select * from SEASONS order by SEASON')
+        ret = []
+        for s in seasons:
+          season = {}
+          season['id'] = s['season_id']
+          season['season'] = s['season']
+          ret.append(season)
+        return json.dumps(ret)
+
+def teams(cursor=None):
+    with sqlite_cursor(cursor) as c:
+        teams = c.execute("select * from TEAMS order by TEAM_ABBREVIATION")
+        ret = []
+        for t in teams:
+          team = {}
+          team['id'] = t['team_id']
+          team['abbr'] = t['team_abbreviation']
+          team['city'] = t['team_city']
+          team['mascot'] = t['team_mascot']
+          ret.append(team)
+        return json.dumps(ret)
+
+def team_scores(teamname,season,cursor=None):
+    with sqlite_cursor(cursor) as c:
+        scores = c.execute('select * from scores where season='+season+' and (habbr="'+teamname+'" or vabbr="'+teamname+'") order by date;')
+        ret = []
+        for s in scores:
+          score = {}
+          home = score['home'] = (s['vabbr']!=teamname)
+          if home==True:
+            score['score'] = s['hpts']
+            score['opp_score'] = s['vpts']
+            score['opp'] = s['vabbr']
+          else:
+            score['score'] = s['vpts']
+            score['opp_score'] = s['hpts']
+            score['opp'] = s['habbr']
+          score['diff'] = score['score'] - score['opp_score']
+          score['gameid'] = s['game_id']
+          ret.append(score)
+        return json.dumps(ret)
+
+def notebook(gameid, cursor=None):
+    with sqlite_cursor(cursor) as c:
+        notebook = c.execute('select notebook from games where game_id='+gameid).fetchone()
+        return notebook[0].replace('\n','\n<br/>')
